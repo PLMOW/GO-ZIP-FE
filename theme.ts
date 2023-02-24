6343fb284dafd645f1c780c190c93cf9181250dd
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-24 16:20:44 +0900

    feat: theme.ts 추가

---
+++ theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from 'styled-components';
+
+export const darkTheme: DefaultTheme = {
+  background: '#2b2b2b',
+  color: '#FADEDD',
+  pointColor: '#FF681B',
+  transitionOption: 'ease-in-out 0.15s',
+};
+
+export const lightTheme: DefaultTheme = {
+  background: '#FADEDD',
+  color: '#2b2b2b',
+  pointColor: '#FF681B',
+  transitionOption: 'ease-in-out 0.15s',
+};
