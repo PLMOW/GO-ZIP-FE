bce805c58c17be2b5266a0b9277138998200a756
bangmookim <tugjbmyhn@naver.com>
2023-02-25 00:20:11 +0900

    feat: Login 컴포넌트 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,44 @@
+import styled from "styled-components";
+import InputBox from "components/form/InputBox";
+
 const Login = () => {
-  return <>Login</>;
+  return (
+    <Wrapper>
+      <Container>
+        <InputBox text="이메일" />
+
+        <InputBox text="비밀번호" />
+        <InputBox text="닉네임" />
+      </Container>
+    </Wrapper>
+  )
 };
 
 export default Login;
+
+
+
+
+
+const Container = styled.div`
+  margin: 0 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 20px;
+  
+  `
+
+
+const Wrapper = styled.div`
+position: fixed;
+margin-top: 70px;
+width: 100vw;
+height: 100vh;
+`
+
+
+
+
+
