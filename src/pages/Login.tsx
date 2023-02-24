71ec711acdfdf92bae49097d4bed46b6b2b27a1a
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-25 05:11:41 +0900

    feat: Login 기능 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,32 +1,125 @@
+import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
-import InputBox from 'components/form/InputBox';
+import loginUser from 'libs/client/api/loginUser';
+import { motion } from 'framer-motion';
+import { FormState } from 'libs/client/types/formType';
+import ReactHookInput from 'components/form/ReactHookInput';
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormState>();
+
+  const onValid = async (data: FormState) => {
+    const { email, password } = data;
+    const res = await loginUser({ email, password });
+
+    console.log(`res : ${res}`);
+  };
+
   return (
-    <Wrapper>
-      <Container>
-        <InputBox text="이메일" />
-        <InputBox text="비밀번호" />
-        <InputBox text="닉네임" />
-      </Container>
-    </Wrapper>
+    <Container>
+      <TopWrapper>
+        <Title>Log in</Title>
+        <Form onSubmit={handleSubmit(onValid)}>
+          <ReactHookInput
+            type="이메일"
+            register={register}
+            errorMessage={errors.email?.message}
+          />
+          <ReactHookInput
+            type="비밀번호"
+            register={register}
+            errorMessage={errors.password?.message}
+          />
+          <Submit>Login</Submit>
+        </Form>
+      </TopWrapper>
+      <BottomWrapper>
+        <SocialText>LogIn with Social</SocialText>
+        <a href="/login">login</a>
+      </BottomWrapper>
+    </Container>
   );
 };
 
 export default Login;
 
-const Container = styled.div`
-  margin: 0 20px;
+const BottomWrapper = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
   flex-direction: column;
-  gap: 20px;
+  justify-content: flex-end;
+  padding-bottom: 5px;
+  gap: 5px;
+  align-items: center;
+  height: 100px;
+  width: 100%;
 `;
 
-const Wrapper = styled.div`
-  position: fixed;
-  margin-top: 70px;
-  width: 100vw;
-  height: 100vh;
+const SocialText = styled.div`
+  margin-bottom: 10px;
+  font-weight: 600;
+  opacity: 0.7;
+`;
+
+const TopWrapper = styled.div`
+  margin-top: 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Container = styled(motion.div)`
+  position: absolute;
+  width: 400px;
+  height: 700px;
+  border-radius: 5px;
+  left: calc(50% - 250px);
+  top: calc(50% - 400px);
+  color: ${({ theme }) => theme.color};
+  box-shadow: 0px 0px 5px ${({ theme }) => theme.color};
+  backdrop-filter: blur(3px);
+  padding: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  transition: ease-in-out 0.15s;
+`;
+
+const Title = styled.div`
+  font-size: 30px;
+  width: 100%;
+  text-align: center;
+  font-weight: 600;
+`;
+
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-top: 50px;
+  margin-bottom: 20px;
+`;
+
+const Submit = styled.button`
+  margin: 30px 0 30px 0;
+  padding: 10px;
+  border-radius: 5px;
+  background: ${({ theme }) => theme.background};
+  color: ${({ theme }) => theme.color};
+  border: 2px solid ${({ theme }) => theme.color};
+  font-size: 20px;
+  transition: ${({ theme }) => theme.transitionOption};
+  :hover {
+    cursor: pointer;
+    background: ${({ theme }) => theme.color};
+    color: ${({ theme }) => theme.background};
+  }
 `;
