9a5a9a3208ccde1a7fd2c28d924f2e1c585fe273
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-24 16:31:58 +0900

    feat: Redux store 생성

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import themeToggleReducer from './modules/themeToggle';
 
 const store = configureStore({
-  reducer: {},
+  reducer: { themeToggleReducer },
 });
 
 export default store;
