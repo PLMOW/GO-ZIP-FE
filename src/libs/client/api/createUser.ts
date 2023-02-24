03c606f59323818e7450aeaf038e7bd4e68fd5df
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-25 01:05:08 +0900

    feat: Axios UserCreate API 함수 작성

---
+++ src/libs/client/api/createUser.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+interface createUserProps {
+  email: string;
+  password: string;
+  nickname: string;
+}
+
+const createUser = async ({ email, password, nickname }: createUserProps) => {
+  const route = `${process.env.REACT_APP_API_BASE_ROUTE}/api/signin`;
+
+  await axios({
+    method: 'post',
+    url: route,
+    data: {
+      email,
+      password,
+      nickname,
+    },
+  });
+};
+
+export default createUser;
