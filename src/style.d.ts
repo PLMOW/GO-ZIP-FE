c0e2ab057d0edd1854bc77afea1988a0b6584586
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-26 04:40:27 +0900

    feat: theme에서 사용할 추가 필드 type 선언

--- src/style.d.ts
+++ src/style.d.ts
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 declare module 'styled-components' {
   export interface DefaultTheme {
     background: string;
+    transparentBackground: string;
     color: string;
+    transparentColor: string;
     pointColor: string;
     transitionOption: string;
   }
