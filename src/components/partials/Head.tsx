a3d9155a3e36336f5cf22b34c653bd27317c79a2
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-25 23:17:51 +0900

    feat: GO-ZIP favicon 설정

--- src/components/partials/Head.tsx
+++ src/components/partials/Head.tsx
@@ -3,10 +3,6 @@ import { Helmet } from 'react-helmet';
 const Head = () => {
   return (
     <Helmet>
-      <meta
-        http-equiv="Content-Security-Policy"
-        content="upgrade-insecure-requests"
-      ></meta>
       <title>GO-ZIP</title>
     </Helmet>
   );
