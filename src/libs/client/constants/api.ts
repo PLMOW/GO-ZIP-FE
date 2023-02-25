f2e244708f9503146cc85cb14a222ffe935ce146
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-25 13:17:43 +0900

    feat: Login 상수 및 Interface 구현

--- src/libs/client/constants/api.ts
+++ src/libs/client/constants/api.ts
@@ -1,3 +1,21 @@
 export const END_POINT = Object.freeze({
   LOG_IN: '/api/login' as const,
 });
+
+type MethodType = 'get' | 'post' | 'put' | 'delete';
+
+interface Method {
+  [key: string]: MethodType;
+}
+
+export const METHOD = Object.freeze<Method>({
+  GET: 'get' as const,
+  POST: 'post' as const,
+  PUT: 'put' as const,
+  DELETE: 'delete' as const,
+});
+
+export const AXIOS_CONFIG = Object.freeze({
+  CREDENTIAL_TRUE: true as const,
+  CREDENTIAL_FALSE: false as const,
+});
