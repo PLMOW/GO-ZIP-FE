a0f26d981ac2387f51e4c7fdc85f72e0823a285f
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-26 04:10:29 +0900

    feat: Carousel 컴포넌트 Home 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,21 @@
+import Carousel from 'components/slider/Carousel';
+import styled from 'styled-components';
 const Home = () => {
-  return <>Home</>;
+  return (
+    <Wrapper>
+      <Carousel />
+      <Cont />
+    </Wrapper>
+  );
 };
 
 export default Home;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  overflow-x: hidden;
+`;
+
+const Cont = styled.div`
+  height: 100vh;
+`;
