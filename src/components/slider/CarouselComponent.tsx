041aa79c44963809e04615a11a3c3c6179676ac3
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-26 03:52:32 +0900

    feat: Slider 컴포넌트 추가

---
+++ src/components/slider/CarouselComponent.tsx
@@ -0,0 +1,34 @@
+import gsap from 'gsap';
+import img1 from 'assets/img/i1.webp';
+import styled from 'styled-components';
+
+interface CarouselComponentProps {
+  imgSrc: string;
+}
+
+const CarouselComponent = ({ imgSrc }: CarouselComponentProps) => {
+  return (
+    <Wrapper>
+      <Image imgSrc={imgSrc} />
+    </Wrapper>
+  );
+};
+
+export default CarouselComponent;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+`;
+
+const Image = styled.div<{ imgSrc: string }>`
+  height: 100vh;
+  width: 100%;
+  object-fit: cover;
+  background-image: linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0),
+      rgba(0, 0, 0, 0.8)
+    ),
+    url(${({ imgSrc }) => imgSrc});
+`;
