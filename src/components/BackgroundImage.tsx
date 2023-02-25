7ac20b049f681959f70175f273c4ee96483cefbd
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-26 04:41:06 +0900

    feat: 배경 컴포넌트 추가

---
+++ src/components/BackgroundImage.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import { ReactNode } from 'react';
+
+interface BackgroundImageProps {
+  src: string;
+  children: ReactNode;
+}
+
+const BackgroundImage = ({ src, children }: BackgroundImageProps) => {
+  return <Wrapper src={src}>{children}</Wrapper>;
+};
+
+export default BackgroundImage;
+
+const Wrapper = styled.div<{ src: string }>`
+  height: 100vh;
+  width: 100%;
+  object-fit: cover;
+  background-image: linear-gradient(
+      210deg,
+      rgba(222, 222, 222, 0),
+      rgba(0, 0, 0, 0.2)
+    ),
+    url(${({ src }) => src});
+`;
