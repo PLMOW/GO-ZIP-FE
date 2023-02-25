c1b5c22e49328c9683849ed3d1a01def7a7baf9b
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-25 15:58:23 +0900

    test: 테스트 컴포넌트 Route 추가

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const Test = () => {
   const clickHandler = async () => {
+    console.log('Send GET Method');
     const res = await axios.get('http://13.125.193.128:8080/api/tryget');
     console.log('res : ', res);
   };
