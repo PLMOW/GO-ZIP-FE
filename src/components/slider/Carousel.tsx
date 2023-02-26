e34b87a86ee65d636a8f7dbc2ff569cc408323de
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-26 16:05:31 +0900

    feat: carousel 컴포넌트 구현

--- src/components/slider/Carousel.tsx
+++ src/components/slider/Carousel.tsx
@@ -1,11 +1,25 @@
-import img1 from 'assets/img/i1.webp';
 import styled from 'styled-components';
 import CarouselComponent from './CarouselComponent';
+import Left from 'components/icons/Left';
+import Right from 'components/icons/Right';
+import { useSelector } from 'react-redux';
+import { RootState } from 'redux/store';
+import { AnimatePresence } from 'framer-motion';
 
 const Carousel = () => {
+  const { index, imgs } = useSelector((state: RootState) => state.carousel);
+
   return (
     <Wrapper>
-      <CarouselComponent imgSrc={img1} />
+      <Left />
+      <Right />
+      <AnimatePresence>
+        {imgs.map((v, i) =>
+          i === index ? (
+            <CarouselComponent key={`${v}=index`} imgSrc={v} />
+          ) : null
+        )}
+      </AnimatePresence>
     </Wrapper>
   );
 };
@@ -13,6 +27,7 @@ const Carousel = () => {
 export default Carousel;
 
 const Wrapper = styled.div`
+  position: relative;
   width: auto;
   height: 100vh;
 `;
