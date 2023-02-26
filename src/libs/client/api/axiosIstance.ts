a5862d4c69ba5e02aad2c6c878b895f59356af20
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-27 01:52:54 +0900

    feat: Axios Instance dotenv config 추가

--- src/libs/client/api/axiosIstance.ts
+++ src/libs/client/api/axiosIstance.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { AXIOS_CONFIG } from 'libs/client/constants/api';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_ROUTE,
