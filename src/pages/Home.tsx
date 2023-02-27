f44386c6bdc89589454d976ea2bab4ace9ce4f2f
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-27 16:43:35 +0900

    feat: Floatnav 적용

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,21 @@
 import Carousel from 'components/slider/Carousel';
 import styled from 'styled-components';
+import Search from './Search';
+import FloatNav from 'components/partials/FloatNav/FloatNav';
+
 const Home = () => {
   return (
-    <Wrapper>
-      <Carousel />
-      <Cont />
-    </Wrapper>
+    <>
+      <FloatNav />
+      <Wrapper>
+        <Carousel />
+        <Content1>1</Content1>
+        <Content2>2</Content2>
+        <Content3>3</Content3>
+        <Content4>4</Content4>
+        <Search />
+      </Wrapper>
+    </>
   );
 };
 
@@ -13,9 +23,25 @@ export default Home;
 
 const Wrapper = styled.div`
   height: 100vh;
-  overflow-x: hidden;
+  overflow: hidden;
 `;
 
-const Cont = styled.div`
+const Content1 = styled.div.attrs({ id: 'content_1' })`
   height: 100vh;
+  background: tomato;
+`;
+
+const Content2 = styled.div.attrs({ id: 'content_2' })`
+  height: 100vh;
+  background: rgba(113, 32, 12);
+`;
+
+const Content3 = styled.div.attrs({ id: 'content_3' })`
+  height: 100vh;
+  background: rgb(58, 25, 202);
+`;
+
+const Content4 = styled.div.attrs({ id: 'content_4' })`
+  height: 100vh;
+  background: wheat;
 `;
