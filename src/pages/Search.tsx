fcc5d9b0f2a31c6b28380c53d20e1ce0c0a29440
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-28 09:51:25 +0900

    style: ESLint에 따른 code-convention 수정

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,44 +1,44 @@
-import { useEffect, useState } from "react";
-import styled from "styled-components";
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 const Search = () => {
-  const structure = ["원룸", "투룸", "아파트", "빌라", "오피스텔"];
+  const structure = ['원룸', '투룸', '아파트', '빌라', '오피스텔'];
 
   const address = [
     {
-      city: "서울시",
+      city: '서울시',
       town: {
-        강남구: ["대치동", "삼성동"],
-        마포구: ["상수동", "합정동"],
-        구로구: ["구로동"],
+        강남구: ['대치동', '삼성동'],
+        마포구: ['상수동', '합정동'],
+        구로구: ['구로동'],
       },
     },
     {
-      city: "대전시",
+      city: '대전시',
       town: {
-        중구: ["용두동", "부사동"],
+        중구: ['용두동', '부사동'],
       },
     },
     {
-      city: "대구시",
+      city: '대구시',
       town: {
-        달서구: ["성당동", "죽전동", "신당동"],
+        달서구: ['성당동', '죽전동', '신당동'],
       },
     },
     {
-      city: "부산시",
+      city: '부산시',
       town: {
-        해운대구: ["송정동"],
-        영도구: ["남항동", "신성동"],
+        해운대구: ['송정동'],
+        영도구: ['남항동', '신성동'],
       },
     },
   ];
 
   const [data, setData] = useState({
-    city: "",
-    town: "",
-    street: "",
-    house_type: "",
+    city: '',
+    town: '',
+    street: '',
+    house_type: '',
   });
 
   const newArr = address.filter((item) => {
@@ -68,7 +68,7 @@ const Search = () => {
     // setData({ ...data, city: value });
     setData((data) => {
       // 중요: 값을 업데이트할 때 `this.state` 대신 `state` 값을 읽어옵니다.
-      return { ...data, city: value, town: "" };
+      return { ...data, city: value, town: '' };
     });
   };
   const addHouseTypeButtonClickHandler = (item: any) => {
@@ -82,68 +82,54 @@ const Search = () => {
     <Body>
       <div
         style={{
-          overflow: "hidden",
-          width: "94%",
-          height: "40vh",
-          border: "1px solid lightgray",
-          padding: "60px 30px",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
+          overflow: 'hidden',
+          width: '94%',
+          height: '40vh',
+          border: '1px solid lightgray',
+          padding: '60px 30px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
         }}
       >
         <div
           style={{
-            fontSize: "1.4rem",
-            fontWeight: "500",
-            letterSpacing: "-.8px",
+            fontSize: '1.4rem',
+            fontWeight: '500',
+            letterSpacing: '-.8px',
           }}
         >
           고승유의 집구하기
         </div>
-
-        {/* <div
-          style={{
-            border: "1px solid lightgray",
-            width: "700px",
-            height: "40px",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            marginTop: "20px",
-          }}
-        >
-          <Input placeholder="지역을 입력하세요"></Input>
-        </div> */}
         <div
           style={{
-            display: "flex",
-            justifyContent: "center ",
-            width: "100%",
-            gap: "20px",
-            marginTop: "20px",
+            display: 'flex',
+            justifyContent: 'center ',
+            width: '100%',
+            gap: '20px',
+            marginTop: '20px',
           }}
         >
           <SelectWrap>
-            <Select onChange={onChangeCityHandler} style={{ outline: "none" }}>
+            <Select onChange={onChangeCityHandler} style={{ outline: 'none' }}>
               <option>--시선택--</option>
               {address?.map((item, i) => {
                 return <option key={i}>{item.city}</option>;
               })}
             </Select>
 
-            <Select onChange={onChangeTownHandler} style={{ outline: "none" }}>
+            <Select onChange={onChangeTownHandler} style={{ outline: 'none' }}>
               <option>--구선택--</option>
               {gu_name[0]?.map((item, i) => {
                 return <option key={i}>{item}</option>;
               })}
             </Select>
-            <Select style={{ outline: "none" }}>
+            <Select style={{ outline: 'none' }}>
               <option>--동선택--</option>
             </Select>
           </SelectWrap>
         </div>
-        <div style={{ display: "flex", marginTop: "20px", gap: "20px" }}>
+        <div style={{ display: 'flex', marginTop: '20px', gap: '20px' }}>
           {structure.map((item, i) => {
             return (
               <Btn key={i} onClick={() => addHouseTypeButtonClickHandler(item)}>
@@ -156,18 +142,18 @@ const Search = () => {
 
       <div
         style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "30px",
+          display: 'flex',
+          justifyContent: 'center',
+          marginTop: '30px',
         }}
       >
         <div
           style={{
-            width: "90vw",
-            display: "flex",
-            alignItems: "flex-start",
-            flexWrap: "wrap",
-            gap: "20px",
+            width: '90vw',
+            display: 'flex',
+            alignItems: 'flex-start',
+            flexWrap: 'wrap',
+            gap: '20px',
           }}
         >
           <SmallImgBox></SmallImgBox>
