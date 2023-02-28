2dffd4d04ef391202b04e6ded7f70d5a240699a8
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-01 05:10:44 +0900

    refactor: Search API 하드코딩 된 값 제거

--- src/libs/client/api/searchByQuery.ts
+++ src/libs/client/api/searchByQuery.ts
@@ -1,10 +1,12 @@
 import api from 'libs/client/api/axiosIstance';
+import { END_POINT, METHOD } from 'libs/client/constants/api';
 
 interface queryType {
   city: string;
   town: string;
   house_type: string;
 }
+
 const searchByQuery = async ({ city, town, house_type }: queryType) => {
   const queryData = {
     city,
@@ -20,11 +22,11 @@ const searchByQuery = async ({ city, town, house_type }: queryType) => {
     .join('&');
 
   const res = await api({
-    method: 'GET',
-    url: `api/product/search?${query}`,
+    method: METHOD.GET,
+    url: END_POINT.SEARCH(query),
   });
 
-  console.log(res);
+  return res;
 };
 
 export default searchByQuery;
