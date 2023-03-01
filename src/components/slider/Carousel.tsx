2debb152420a1a1c43c044f61b27e101e2c0ac26
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-01 17:52:55 +0900

    feat: Carousel setInterval Animation 최적화

--- src/components/slider/Carousel.tsx
+++ src/components/slider/Carousel.tsx
@@ -1,11 +1,27 @@
 import styled from 'styled-components';
 import CarouselComponent from './CarouselComponent';
-import { useSelector } from 'react-redux';
 import { RootState } from 'redux/store';
 import { AnimatePresence } from 'framer-motion';
+import { next } from 'redux/modules/carousel';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import useInterval from 'hooks/useInterval';
 
 const Carousel = () => {
-  const { index, imgs } = useSelector((state: RootState) => state.carousel);
+  const dispatch = useDispatch();
+  const { index, imgs, intervalRef } = useSelector(
+    (state: RootState) => state.carousel
+  );
+  const carouselInterval = useInterval(
+    () => dispatch(next()),
+    7000,
+    intervalRef
+  );
+
+  /* Handle Carousel Infinity Animate */
+  useEffect(() => {
+    carouselInterval();
+  }, []);
 
   return (
     <Wrapper>
